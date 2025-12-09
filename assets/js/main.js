1fb5aed8696ba3e4412237661da30c7aa6b1e31c
Celine-Poloni <celine.poloni@gmail.com>
2025-12-09 21:24:28 +0100

    feat: create hero banner with cta button and image

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,8 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "shadow-md",
         "text-gray-700",
         "backdrop-blur-lg",
-        "py-3",
-        "md:py-4"
+        "py-2",
+        "md:py-3"
       );
       navbar.classList.remove("py-4", "md:py-6");
       
@@ -66,8 +66,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "shadow-md",
         "text-gray-700",
         "backdrop-blur-lg",
-        "py-3",
-        "md:py-4"
+        "py-2",
+        "md:py-3"
       );
       navbar.classList.add("py-4", "md:py-6");
       
